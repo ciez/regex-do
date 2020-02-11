9b7d2184a96ca9c41c3fcce58641a93767caad5f
Imants Cekusins <imantc@gmail.com>
2020-02-11 03:06:46 +0200

    doc

--- doc.sh
+++ doc.sh
@@ -1,1 +1,5 @@
-cabal haddock --executables
+# https://hackage.haskell.org/upload
+
+# cabal v2-haddock --builddir="dist-doc" --haddock-for-hackage --enable-doc
+
+# cabal upload -d --publish dist-doc/*-docs.tar.gz
--- doc2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# gen haddock for package
-cabal haddock --hoogle --html-location=doc
